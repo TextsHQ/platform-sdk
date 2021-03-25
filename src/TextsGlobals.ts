205ee032fbee4e78ba3f4b43f8c53182c67b4b4b
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-03-26 00:12:04 +0530

    refactor

---
+++ src/TextsGlobals.ts
@@ -0,0 +1,22 @@
+import type SentryNode from '@sentry/node'
+import type React from 'react'
+import type { Worker } from 'worker_threads'
+
+export type TextsGlobals = {
+  IS_DEV: boolean
+
+  log: (...args: any[]) => void
+  error: (...args: any[]) => void
+
+  constants: {
+    USER_AGENT: string
+    APP_VERSION: string
+    BUILD_DIR_PATH: string
+  }
+
+  Sentry: typeof SentryNode
+  React?: typeof React
+
+  trackPlatformEvent?: (data: any) => Promise<void>
+  runWorker?: (workerFilePath: string, workerData: any) => Worker
+}
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,18 @@
-import type SentryNode from '@sentry/node'
-import type React from 'react'
-import type { Worker } from 'worker_threads'
+import type { TextsGlobals } from './TextsGlobals'
 
 export * from './enums'
 export * from './errors'
-export * from './types'
 export * from './constants'
 
-export type TextsGlobals = {
-  IS_DEV: boolean
-
-  log: (...args: any[]) => void
-  error: (...args: any[]) => void
-
-  constants: {
-    USER_AGENT: string
-    APP_VERSION: string
-    BUILD_DIR_PATH: string
-  }
-
-  Sentry: typeof SentryNode
-  React?: typeof React
-
-  trackPlatformEvent?: (data: any) => Promise<void>
-  runWorker?: (workerFilePath: string, workerData: any) => Worker
-}
+export * from './generic'
+export * from './Message'
+export * from './PhoneNumber'
+export * from './Platform'
+export * from './PlatformAPI'
+export * from './PlatformInfo'
+export * from './ServerEvent'
+export * from './TextAttributes'
+export * from './Thread'
+export * from './User'
 
 export const texts = (globalThis as any).texts as TextsGlobals

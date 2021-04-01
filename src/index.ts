6150df5a41f8b1d4ac2ee2e565d7aac6d45674cb
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-04-02 01:15:52 +0530

    export ThreadFolderName

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export * from './PlatformInfo'
 export * from './ServerEvent'
 export * from './TextAttributes'
 export * from './Thread'
+export * from './ThreadFolderName'
 export * from './User'
 
 export const texts = (globalThis as any).texts as TextsGlobals

36f3b4f9b34125b7790a36883e68382f6f2b96ad
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-05-20 20:26:31 +0530

    Attribute.SUPPORTS_EDIT_MESSAGE

--- src/enums.ts
+++ src/enums.ts
@@ -12,6 +12,7 @@ export enum Attribute {
   CAN_MESSAGE_USERNAME = 'can_message_username',
   /** Don't cache messages, threads, users for the platform */
   NO_CACHE = 'no_cache',
+
   /** Platform doesn't allow creation of duplicate groups with the same set of users (Alice, Bob) */
   NO_SUPPORT_DUPLICATE_GROUP_CREATION = 'no_support_duplicate_group_creation',
   /** Platform integration doesn't support adding participants to groups */
@@ -26,25 +27,29 @@ export enum Attribute {
   NO_SUPPORT_SINGLE_THREAD_CREATION = 'no_support_single_thread_creation',
   /** Platform integration doesn't support sending typing indicator */
   NO_SUPPORT_TYPING_INDICATOR = 'no_support_typing_indicator',
+
   SHARES_CONTACTS = 'shares_contacts',
   /** Sort messages by timestamp or custom key on receiving them */
   SORT_MESSAGES_ON_PUSH = 'sort_messages_on_push',
   SUBSCRIBE_TO_CONN_STATE_CHANGE = 'subscribe_to_conn_state_change',
   SUBSCRIBE_TO_THREAD_SELECTION = 'subscribe_to_thread_selection',
+
   SUPPORTS_ARCHIVE = 'supports_archive',
+  SUPPORTS_DELETE_THREAD = 'supports_delete_thread',
+  /** Platform integration implements PlatformAPI.editMessage */
+  SUPPORTS_EDIT_MESSAGE = 'supports_edit_message',
   SUPPORTS_FORWARD = 'supports_forward',
   SUPPORTS_GROUP_IMAGE_CHANGE = 'supports_group_image_change',
   SUPPORTS_GROUP_PARTICIPANT_ROLE_CHANGE = 'supports_group_participant_role_change',
   SUPPORTS_MARK_AS_UNREAD = 'supports_mark_as_unread',
   SUPPORTS_PIN_THREAD = 'supports_pin_thread',
-  SUPPORTS_DELETE_THREAD = 'supports_delete_thread',
   SUPPORTS_PRESENCE = 'supports_presence',
   SUPPORTS_QUOTED_MESSAGES = 'supports_quoted_messages',
   SUPPORTS_REQUESTS_INBOX = 'supports_requests_inbox',
   SUPPORTS_SEARCH = 'supports_search',
   SUPPORTS_STOP_TYPING_INDICATOR = 'supports_stop_typing_indicator',
 
-  /** Platform integration supports editMessage() and has no rate limit */
+  /** Platform integration implements editMessage() and has no rate limit for message edits */
   SUPPORTS_LIVE_TYPING = 'supports_live_typing',
 }
 

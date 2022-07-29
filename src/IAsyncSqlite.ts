9e4a804602049ed46a3389ffcac1d6bc43eaa288
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-07-29 13:26:35 +0530

    improve IAsyncSqlite typing

--- src/IAsyncSqlite.ts
+++ src/IAsyncSqlite.ts
@@ -1,4 +1,5 @@
 // taken from better-sqlite3
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/better-sqlite3/index.d.ts
 export interface DBOptions {
   readonly?: boolean | undefined
   fileMustExist?: boolean | undefined
@@ -6,20 +7,24 @@ export interface DBOptions {
   verbose?: ((message?: any, ...additionalArgs: any[]) => void) | undefined
 }
 
+interface RunResult {
+  changes: number
+  lastInsertRowid: number | bigint
+}
 export interface IAsyncSqlite {
   new(): this
 
   init(filename: string, options?: DBOptions): Promise<void>
 
-  exec(sql: string): Promise<any>
+  exec(sql: string): Promise<boolean>
 
-  all(sql: string, bindParameters?: any[]): Promise<any>
-  get(sql: string, bindParameters?: any[]): Promise<any>
-  run(sql: string, bindParameters?: any[]): Promise<any>
+  run<BindParameters extends any[]>(sql: string, ...params: BindParameters): Promise<RunResult>
+  all<BindParameters extends any[], ResultType>(sql: string, ...params: BindParameters): Promise<ResultType[]>
+  get<BindParameters extends any[], ResultType>(sql: string, ...params: BindParameters): Promise<ResultType>
 
-  raw_all(sql: string, bindParameters?: any[]): Promise<any>
-  pluck_all(sql: string, bindParameters?: any[]): Promise<any>
-  pluck_get(sql: string, bindParameters?: any[]): Promise<any>
+  raw_all<BindParameters extends any[], ResultType>(sql: string, ...params: BindParameters): Promise<ResultType[]>
+  pluck_all<BindParameters extends any[], ResultType>(sql: string, ...params: BindParameters): Promise<ResultType[]>
+  pluck_get<BindParameters extends any[], ResultType>(sql: string, ...params: BindParameters): Promise<ResultType>
 
   dispose(): Promise<void>
 }

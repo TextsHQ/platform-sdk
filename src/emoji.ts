823e1e5c40ff4c321b88fe271f6fd9f08746e181
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-08-28 18:42:28 +0530

    isEmojiOrSpacesOnlyString

---
+++ src/emoji.ts
@@ -0,0 +1,17 @@
+import getEmojiRegex from 'emoji-regex'
+
+const emojiRegex = getEmojiRegex()
+const whitespaceRegexGlobal = /[\s\u200B-\u200D\uFEFF]+/g
+
+// export const isEmojiOnlyString = (string: string) =>
+//   typeof string === 'string'
+//     && string.replace(emojiRegex, '')
+//       .length === 0
+
+export const isEmojiOrSpacesOnlyString = (string: string): boolean =>
+  typeof string === 'string'
+    && string.length > 0
+    && string
+      .replace(emojiRegex, '')
+      .replace(whitespaceRegexGlobal, '')
+      .length === 0

2bb06c55d2982c119f97ab18f579656a8e7fbba7
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-10-07 18:44:06 +0530

    fix error names

--- src/errors.ts
+++ src/errors.ts
@@ -1,9 +1,12 @@
 
 export class ReAuthError extends Error { }
+ReAuthError.prototype.name = 'ReAuthError'
 
 export class RateLimitError extends Error { }
+RateLimitError.prototype.name = 'RateLimitError'
 
 export class ConnectionError extends Error { }
+ConnectionError.prototype.name = 'ConnectionError'
 
 export enum PlatformErrorName {
   'ReAuthError' = 'ReAuthError',

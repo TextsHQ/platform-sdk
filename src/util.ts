a42fae668979080be60dd8fad5a43c0c810734b1
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-12-30 15:41:25 +0530

    parseMessageTemplate: fix emoji/unicode parsing

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,10 @@ import type { TextEntity } from './TextAttributes'
 
 export function parseMessageTemplate(text: string, senderID: string): TextEntity[] {
   const regex = /{{(.*?)}}/g
-  return [...text.matchAll(regex)]
-    .map<TextEntity>(m => ({ from: m.index!, to: m.index! + m[0].length, mentionedUser: { id: m[1] === 'sender' ? senderID : m[1] } }))
+  const toScalarIndex = (idx: number) => Array.from(text.substring(0, idx)).length
+  return [...text.matchAll(regex)].map<TextEntity>(m => ({
+    from: toScalarIndex(m.index!),
+    to: toScalarIndex(m.index! + m[0].length),
+    mentionedUser: { id: m[1] === 'sender' ? senderID : m[1] },
+  }))
 }

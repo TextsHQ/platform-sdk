acd5121ea55ca4f1bcbc42cf2380396d31e6e862
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-12-22 01:55:28 +0530

    first

---
+++ src/PersistentWS.ts
@@ -0,0 +1,80 @@
+import { setTimeout as sleep } from 'timers/promises'
+import WebSocket from 'ws'
+import { debounce } from 'lodash'
+import { texts } from '.'
+
+const MAX_RETRY_ATTEMPTS = Infinity
+const getRetryTimeout = (attempt: number) =>
+  Math.min(100 + (2 ** attempt + Math.random() * 100), 60_000)
+
+const WAIT_DELAY_MS = 50
+// similar to texts-app-desktop/src/renderer/ws-transport.ts
+export default class PersistentWS {
+  private ws: WebSocket | undefined
+
+  private disposing = false
+
+  private retryAttempt = 0
+
+  private connectTimeout: ReturnType<typeof setTimeout> | undefined
+
+  constructor(
+    private readonly endpoint: string,
+    private readonly options: WebSocket.ClientOptions,
+    private readonly onMessage: (msg: Buffer) => void,
+  ) {}
+
+  readonly connect = async () => {
+    try {
+      this.dispose()
+    } catch (err) {
+      texts.error('[PersistentWS] connect', err)
+    }
+    const retry = debounce(() => {
+      if (++this.retryAttempt <= MAX_RETRY_ATTEMPTS) {
+        clearTimeout(this.connectTimeout!)
+        this.connectTimeout = setTimeout(this.connect, getRetryTimeout(this.retryAttempt))
+      } else {
+        this.disposing = true
+      }
+    }, 25)
+    texts.log('[PersistentWS] connecting', this.endpoint)
+    this.ws = new WebSocket(this.endpoint, this.options)
+    this.ws
+      .on('open', () => {
+        texts.log('[PersistentWS] open')
+        this.disposing = false
+        this.retryAttempt = 0
+      })
+      .on('message', this.onMessage)
+      .on('close', code => {
+        texts.log('[PersistentWS] close', { code })
+        if (!this.disposing) retry()
+        else this.disposing = false
+      })
+      .on('error', error => {
+        console.error('[PersistentWS] error', error)
+        if (!this.disposing) retry()
+        else this.disposing = false
+      })
+  }
+
+  private readonly waitAndSend = async (data: any): Promise<void> => {
+    while (!this.ws || (this.ws?.readyState !== this.ws?.OPEN)) {
+      texts.log(`[PersistentWS] waiting ${WAIT_DELAY_MS}ms`, data)
+      await sleep(WAIT_DELAY_MS)
+    }
+    this.send(data)
+  }
+
+  readonly send = (data: any): Promise<void> | undefined => {
+    if (!this.ws || (this.ws?.readyState !== this.ws?.OPEN)) return this.waitAndSend(data)
+    this.ws!.send(data)
+  }
+
+  dispose() {
+    if (!this.ws) return
+    this.disposing = true
+    this.ws!.close()
+  }
+}
